0be80d4e95d51ceab2e232dae356fb876f14c95c
Mamadaliev Dilshodjon <88366209+DilshodbekMX@users.noreply.github.com>
2022-04-07 10:23:46 +0500

    Update settings.py

--- loop_attendance/settings.py
+++ loop_attendance/settings.py
@@ -12,7 +12,7 @@ LOGIN_REDIRECT_URL = '/login/'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q)u1*0c(p#%5ba@w-)zz#he+7ytn5e!@wg&rd*tlk$n)g!54nm'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
